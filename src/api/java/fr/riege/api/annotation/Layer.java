30e1d8b94bff7c387d2876f1ab184f96a55b88a0
riege <riegertvictor@gmail.com>
2026-04-01 15:18:05 +0200

    feat(api): add @Layer marker annotation

---
+++ src/api/java/fr/riege/api/annotation/Layer.java
@@ -0,0 +1,14 @@
+package fr.riege.api.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Layer {
+    String value() default "";
+}
